90a3e0b59a8038d4990c9a840547e3911df2ec5b
aishwaryayuvaraj <yuvarajaishwarya@gmail.com>
2025-05-19 01:18:02 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, result;
+    int num1, num2, r;
 
     // Asking for user input
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
 
     // Adding the two numbers
-    result = num1 + num2;
+    r = num1 + num2;
 
-    // Displaying the result
+    // Displaying the r
     printf("result: %d\n", result);
 
     return 0;
