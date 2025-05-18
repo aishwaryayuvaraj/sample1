95f85714155d53ba2e2c07b50e8cc8d029b8184c
aishwarya yuvaraj <yuvarajaishwarya@gmail.com>
2025-05-19 00:29:52 +0530

    replace r with result

--- add.c
+++ add.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, r;
+    int num1, num2, result;
 
     // Asking for user input
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
 
     // Adding the two numbers
-    r = num1 + num2;
+    result = num1 + num2;
 
     // Displaying the result
-    printf("result: %d\n", r);
+    printf("result: %d\n", result);
 
     return 0;
 }
